6a40a09e4062e5fd0c349e373d812f83846589d4
lucianzhong <lucianzhong@gmail.com>
2019-01-21 10:46:26 +0800

    2019-01-21

--- DQN_TORCS.py
+++ DQN_TORCS.py
@@ -23,11 +23,9 @@ ACTIONS = 3 				# number of valid actions
 GAMMA = 0.99 				# decay rate of past observations
 OBSERVE = 10 			# timesteps to observe before training
 EXPLORE = 20. 		    # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 		# final value of epsilon
-INITIAL_EPSILON = 0.001    # starting value of epsilon
+
 REPLAY_MEMORY = 50 		# number of previous transitions to remember
 BATCH = 5 					# size of minibatch
-FRAME_PER_ACTION = 1
 ##########################################################################################################################################################
 # 权重
 def weight_variable(shape):
@@ -64,19 +62,19 @@ def createNetwork():
     s = tf.placeholder("float", [None, 80, 80, 4])					# 
     #print("s.shape",s.shape)
     # 第一个隐藏层+一个池化层
-    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)			#  
+    h_conv1 = tf.nn.tanh(conv2d(s, W_conv1, 4) + b_conv1)			#  
     h_pool1 = max_pool_2x2(h_conv1)									# 
     #print("h_pool1.shape",h_pool1.shape)
     #第二个隐藏层
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)		#   
+    h_conv2 = tf.nn.tanh(conv2d(h_pool1, W_conv2, 2) + b_conv2)		#   
     # 第三个隐藏层
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)		# 
+    h_conv3 = tf.nn.tanh(conv2d(h_conv2, W_conv3, 1) + b_conv3)		# 
     #print("h_conv3.shape",h_conv3.shape)
     #展平
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
     #print("h_conv3_flat.shape",h_conv3_flat.shape)
     # 第一个全连接层
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+    h_fc1 = tf.nn.tanh(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
     #print("h_fc1.shape",h_fc1.shape)
     # readout layer  输出层
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
@@ -99,10 +97,10 @@ def trainNetwork(s, readout, h_fc1, sess):
     # store the previous observations in replay memory
     D = deque()
     # start training
-    epsilon = INITIAL_EPSILON
+    epsilon = 0.1
     for episode in range(EPISODE):
         # open up a game state to communicate with emulator
-        env = TorcsEnv(vision=True, throttle=False,gear_change=False)
+        env = TorcsEnv(vision=True, throttle=True,gear_change=False)
         if np.mod(episode, 3) == 0:
             ob = env.reset(relaunch=True)   #relaunch TORCS every 3 episode because of the memory leak error
         else:
@@ -111,7 +109,6 @@ def trainNetwork(s, readout, h_fc1, sess):
         if episode==0:
             # get the first state by doing nothing and preprocess the image to 80x80x4
             do_nothing = np.zeros(ACTIONS)
-            do_nothing[0] = 1
             ob, r_0, terminal, info = env.step(do_nothing)
             x_t=ob.img
             print("type(x_t)",type(x_t))
@@ -123,64 +120,48 @@ def trainNetwork(s, readout, h_fc1, sess):
             x_t=cv2.resize(x_t, (80, 80))
             print("x_t.shape",x_t.shape)
             x_t = cv2.cvtColor(x_t, cv2.COLOR_BGR2GRAY)  
-
             ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)  #对图像进行二值化,从灰度图像中获取二进制图像或用于消除噪声，即滤除太小或太小的像素
             s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)  # 将图像处理成4通道,stack last 4 frames to produce an 80x80x4 input array for network
-
-
 
         for step in range(STEP):
             # choose an action epsilon greedily
             readout_t = readout.eval(feed_dict={s : [s_t]})[0]   #将当前环境输入到CNN网络中
+            print("readout_t",readout_t)
             a_t = np.zeros([ACTIONS])
-            action_index = 0
-
-            if step % FRAME_PER_ACTION == 0:
-                if random.random() <= epsilon:
-                    print("----------Random Action----------")
-                    action_index = random.randrange(ACTIONS)
-                    a_t[random.randrange(ACTIONS)] = 1
-                else:
-                    action_index = np.argmax(readout_t)
-                    a_t[action_index] = 1
+            if random.random() <= epsilon:
+                print("Random Action")
+                a_t[0] = random.random()
+                a_t[1] = random.random()
+                a_t[2] = random.random()
             else:
-                a_t[0] = 1 # do nothing
-
-            # scale down epsilon
-            if epsilon > FINAL_EPSILON and step > OBSERVE:
-                epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
-
+                a_t[0] = readout_t[0]
+                a_t[1] = readout_t[1]
+                a_t[2] = readout_t[2]
             # run the selected action and observe next state and reward
-            #  x_t1_colored, r_t, terminal = game_state.frame_step(a_t)                   # 执行选择的动作，并保存返回的状态、得分
             ob, r_t, terminal, info = env.step(a_t)
             x_t1_colored=ob.img
             x_t1_colored=x_t1_colored.swapaxes(0,2)
             x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
             ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
-
             x_t1 = np.reshape(x_t1, (80, 80, 1))
             s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
-
-            # store the transition in D
-            #   经验池保存的是以一个马尔科夫序列于D中
+            # store the transition in D,经验池保存的是以一个马尔科夫序列于D中
             D.append((s_t, a_t, r_t, s_t1, terminal))
         
             if len(D) > REPLAY_MEMORY:
                 D.popleft()
-
             # only train if done observing
-            if step > OBSERVE:  #OBSERVE = 100000.# timesteps to observe before training
+            if step > OBSERVE:  # timesteps to observe before training
                 # sample a minibatch to train on
-                minibatch = random.sample(D, BATCH)  #32
-
+                minibatch = random.sample(D, BATCH)
                 # get the batch variables
                 s_j_batch = [d[0] for d in minibatch]
                 a_batch = [d[1] for d in minibatch]
                 r_batch = [d[2] for d in minibatch]
                 s_j1_batch = [d[3] for d in minibatch]
-
                 readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
                 y_batch = []  #y_batch表示标签值，如果下一时刻游戏关闭则直接用奖励做标签值，若游戏没有关闭，则要在奖励的基础上加上GAMMA比例的下一时刻最大的模型预测值
+
                 for i in range(0, len(minibatch)):
                     terminal = minibatch[i][4]
                     # if terminal, only equals reward
@@ -207,7 +188,7 @@ def trainNetwork(s, readout, h_fc1, sess):
             else:
                 state = "train"
 
-            print("TIMESTEP", step, "/ STATE", state,  "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t,  "/ Q_MAX %e" % np.max(readout_t))
+            print("TIMESTEP", step, "/ STATE", state,  "/ EPSILON", epsilon, "/ REWARD", r_t,  "/ Q_MAX %e" % np.max(readout_t))
 
 
 
